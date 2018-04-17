992b3e163e1f4d8a786673923f6e30103303f617
AnnieChase972 <37759899+AnnieChase972@users.noreply.github.com>
2018-04-16 21:21:50 -0400

    Update arrays.c

--- arrays.c
+++ arrays.c
@@ -10,18 +10,16 @@ main()
     for (i = 0; i < 10; ++i)
         ndigit[i] = 0;
         
-    while ((c = getchar()) ! = EOF)
+    while ((c = getchar()) != EOF)
         if (c >= '0' && c <= '9')
             ++ndigit[c-'0'];
         else if (c == ' ' || c == '\n' || c == '\t')
             ++nwhite;
         else
-            ++nother
+            ++nother;
     printf("digits =");
-    from (i =0; i < 10; ++i)
+    for (i =0; i < 10; ++i);
         printf(" %d", ndigit[i]);
     printf(", white space = %d, other= %d\n",
         nwhite, nother);
 }
-        
-    
